dad08407962115defe0d483807afb0091620fcab
hci001030 <hci001030@gmail.com>
2026-04-06 14:40:05 +0000

    Initialize project with core files

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-// Add JS here
+const lottoNumbersContainer = document.querySelector('.lotto-numbers');
+const generateBtn = document.querySelector('#generate-btn');
+
+function generateLottoNumbers() {
+    lottoNumbersContainer.innerHTML = '';
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        numbers.add(Math.floor(Math.random() * 45) + 1);
+    }
+
+    const sortedNumbers = Array.from(numbers).sort((a, b) => a - b);
+
+    for (const number of sortedNumbers) {
+        const numberElement = document.createElement('div');
+        numberElement.classList.add('lotto-number');
+        numberElement.textContent = number;
+        lottoNumbersContainer.appendChild(numberElement);
+    }
+}
+
+generateBtn.addEventListener('click', generateLottoNumbers);
+
+generateLottoNumbers();
